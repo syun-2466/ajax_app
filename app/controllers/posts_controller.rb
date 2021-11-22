e88099550cf10abe37aae7eb37a9fcb5da556d47
syun-2466 <chip.nattu@icloud.com>
2021-11-22 12:44:16 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,10 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
 
-  #def new
-  #end
-
   def create
     post = Post.create(content: params[:content])
-    redirect_to json: {post: post}
+    render json:{ post: post }
   end
 end
